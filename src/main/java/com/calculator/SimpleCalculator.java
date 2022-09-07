60f6058c963ed9a2eaf115a2ff8e1b1a54847fa6
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-07 18:44:33 +0530

    created the getter and setter methods for the SimpleCalculator.java class

--- src/main/java/com/calculator/SimpleCalculator.java
+++ src/main/java/com/calculator/SimpleCalculator.java
@@ -16,4 +16,20 @@ public class SimpleCalculator{
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
     }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
 }
